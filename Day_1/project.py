ca99768319c4060d924ad267e8955579d10c1c26
Niklas Lengert <s0563290@htw-berlin.de>
2025-04-22 15:28:52 +0200

    first commit

---
+++ Day_1/project.py
@@ -0,0 +1,6 @@
+#Project Day 1 Band Name Generator
+
+print("Hello. This program will ask you two questions and then create a Band Name out of the answers.")
+city = input("In which city did you grow up?\n")
+pet = input("If you have or would have a pet what is or would be the name of the pet?\n")
+print("This would be your band name: " + city + " " + pet)
---
+++ Day_1/task.py
@@ -0,0 +1,24 @@
+
+#Practice
+#print("Hello World")
+
+#print("Hello World\nHello World")
+
+#print("Hello " + "Niklas")
+
+#input("What is your name?")
+
+#print("Hello " + input("What is your name?"))
+
+#name = input("What is your name?")
+#print(name)
+
+# name = "A"
+# print(name)
+
+# name = "B"
+# print(name)
+
+# userInput = input("Please type something.")
+# length_userInput = len(userInput)
+# print(length_userInput)
