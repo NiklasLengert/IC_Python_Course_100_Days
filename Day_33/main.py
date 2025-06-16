005494d0900cc5f99cc3a5c9a754edc1d89de679
NiklasLengert <niklas.lengert@gmail.com>
2025-06-16 10:46:20 +0200

    Urlaub

---
+++ Day_33/main.py
@@ -0,0 +1,22 @@
+import requests
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# if response.status_code != 200:
+#     raise Exception("Bad response from ISS API") 
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+# print(iss_position) 
+
+# response = requests.get(url="https://api.kanye.rest")
+# response.raise_for_status()
+# data = response.json()["quote"]
+# print(data)
